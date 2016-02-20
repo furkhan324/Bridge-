8fdf93422a612cf43efeb71202eb25d85984f659
furkhan324 <furkhan324@gmail.com>
2016-02-19 20:40:44 -0800

    Update Add.java

--- app/src/main/java/com/example/mohammed/bridge/Add.java
+++ app/src/main/java/com/example/mohammed/bridge/Add.java
@@ -1,6 +1,6 @@
 package com.example.mohammed.bridge;
 //class that adds events to bridge
-//takes user input added modal support as of 
+//takes user input added modal support as 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
